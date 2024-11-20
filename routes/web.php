d39a2738312764f62a475ddef8dc32496dc823d4
KASRO1 <nikita150489@mail.ru>
2024-11-21 02:27:27 +0500

    first commit1

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,41 @@ Route::get('/contact', [PageController::class, 'contacts']);
 Route::get('/privacy-policy', [PageController::class, 'privacyPolicy']);
 Route::get('/terms-of-use', [PageController::class, 'termsOfUse']);
 
+// Маршрут для обработки статических страниц в папке post
+Route::get('post/{page}', function($page) {
+    $file = public_path("post/{$page}.html");
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    abort(404);
+})->where('page', '.*');
+
+// Маршрут для обработки статических страниц в папке direction
+Route::get('direction/{page}', function($page) {
+    $file = public_path("direction/{$page}.html");
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    abort(404);
+})->where('page', '.*');
+
+// Маршрут для обработки статических страниц в папке blog
+Route::get('blog/{page}', function($page) {
+    $file = public_path("blog/{$page}.html");
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    abort(404);
+});
+
+Route::get('{page}', function($page) {
+    $file = public_path("{$page}.html");
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    abort(404);
+})->where('page', '.*');
+
 
 
 
